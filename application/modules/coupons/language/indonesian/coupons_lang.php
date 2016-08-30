8b45804684c4687532a401dd6596f04ace2faed7
Hasan Setiawan <hasan.setiawan@kodefox.com>
2016-08-30 14:00:04 +0700

    add translation to kupon

--- application/modules/coupons/language/indonesian/coupons_lang.php
+++ application/modules/coupons/language/indonesian/coupons_lang.php
@@ -1,37 +1,37 @@
 <?php
 /******************************************
-US English
+INDONESIAN
 Admin Coupon Language
 ******************************************/
 
-$lang['coupons'] = 'Coupons';
-$lang['coupon_form'] = 'Coupon Form';
-$lang['error_not_found'] = 'The requested coupon could not be found.';
-$lang['no_coupons'] = 'There are currently no coupons.';
-$lang['confirm_delete_coupon'] = 'Are you sure you want to delete this coupon?';
-$lang['add_new_coupon'] = 'Add New Coupon';
-$lang['code'] = 'Code';
-$lang['max_uses'] = 'Max Uses';
-$lang['whole_order_discount'] = 'Whole Order Discount?';
-$lang['reduction_type'] = 'Reduction Type';
-$lang['reduction_amount'] = 'Reduction Amount';
-$lang['start_date'] = 'Start Date (UTC)';
-$lang['end_date'] = 'End Date (UTC)';
-$lang['message_saved_coupon'] = 'The coupon has been saved!';
-$lang['error_already_used'] = 'The requested coupon code is already in use!';
-$lang['message_coupon_deleted'] = 'The coupon has been deleted.';
-$lang['attributes'] = 'Attributes';
-$lang['applies_to'] = 'Applies To';
-$lang['coupon_code'] = 'Coupon Code';
-$lang['times_used'] = 'This coupon has been used %d times.';
-$lang['limit_per_order'] = 'Limit Per Order';
-$lang['enable_on'] = 'Enable On (UTC)';
-$lang['disable_on'] = 'Disable On (UTC)';
-$lang['coupon_type'] = 'Coupon Type';
-$lang['price_discount'] = 'Price Discount';
-$lang['free_shipping'] = 'FREE Shipping';
-$lang['apply_to_whole_order'] = 'Apply Coupon to Whole Order';
-$lang['apply_to_select_items'] = 'Apply Coupon to Select Items';
-$lang['usage'] = 'Usage';
-$lang['percentage'] = 'Percentage';
-$lang['fixed'] = 'Fixed';
+$lang['coupons'] = 'Kupon';
+$lang['coupon_form'] = 'Form Kupon';
+$lang['error_not_found'] = 'Kupon yang diminta tidak ditemukan.';
+$lang['no_coupons'] = 'Saat ini belum ada kupon yang tersedia.';
+$lang['confirm_delete_coupon'] = 'Anda yakin akan menghapus kupon?';
+$lang['add_new_coupon'] = 'Tambah Kupon Baru';
+$lang['code'] = 'Kode';
+$lang['max_uses'] = 'Maksimal Penggunaan';
+$lang['whole_order_discount'] = 'Suluruh Potongan Pesanan?';
+$lang['reduction_type'] = 'Tipe potongan';
+$lang['reduction_amount'] = 'Jumlah Potongan';
+$lang['start_date'] = 'Tanggal Mulai (UTC)';
+$lang['end_date'] = 'Tanggal Berakhir (UTC)';
+$lang['message_saved_coupon'] = 'Kupon berhasil disimpan!';
+$lang['error_already_used'] = 'Kode kupon yang diminta sudah digunakan!';
+$lang['message_coupon_deleted'] = 'Kupon berhasil dihapus.';
+$lang['attributes'] = 'Attribut';
+$lang['applies_to'] = 'Kenakan Pada';
+$lang['coupon_code'] = 'Kode Kupon';
+$lang['times_used'] = 'Kupon ini sudah dipakai %d kali.';
+$lang['limit_per_order'] = 'Limit per Pesanan';
+$lang['enable_on'] = 'Aktifkan Pada (UTC)';
+$lang['disable_on'] = 'Nonaktifkan Pada (UTC)';
+$lang['coupon_type'] = 'Tipe Kupon';
+$lang['price_discount'] = 'Harga diskon';
+$lang['free_shipping'] = 'Gratis Pengiriman';
+$lang['apply_to_whole_order'] = 'Terapkan kupon pada semua pesanan';
+$lang['apply_to_select_items'] = 'Terapkan kupon pada item yang terpilih';
+$lang['usage'] = 'Penggunaan';
+$lang['percentage'] = 'Presentase';
+$lang['fixed'] = 'Tetap';
